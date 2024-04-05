f5c9d6ad77cb80b11f7f64994ea132955c38cc7b
DawHom <s27814@pjwstk.edu.pl>
2024-04-05 22:23:07 +0200

    init

---
+++ src/main/java/pj/pjatk/dawlit/DawlitApplication.java
@@ -0,0 +1,13 @@
+package pj.pjatk.dawlit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawlitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawlitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pj/pjatk/dawlit/DawlitApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.pjatk.dawlit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawlitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
